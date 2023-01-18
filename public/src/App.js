0729ab3fe374d605fb904bcddcd18163ffd0adda
Partha Chakraborty <partha.c1999@gmail.com>
2023-01-18 10:27:55 +0530

    GET and POST logic added also used firebase for storing

--- public/src/App.js
+++ public/src/App.js
@@ -13,23 +13,22 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://react-http-practice-a4548-default-rtdb.firebaseio.com/movies.json');
+      const response = await fetch('https://swapi.dev/api/films/');
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
-      const loadedMovies=[];
-      for(const key in data){
-        loadedMovies.push({
-          id:key,
-          title:data[key].title,
-          openingText:data[key].openingText,
-          releaseDate:data[key].releaseDate
-        })
-      }
-      
-      setMovies(loadedMovies);
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -40,16 +39,8 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  async function addMovieHandler(movie) {
-    const response = await fetch('https://react-http-practice-a4548-default-rtdb.firebaseio.com/movies.json',{
-      method:'POST',
-      body:JSON.stringify(movie),
-      headers:{
-        'Content-Type':'application/json'
-      }
-    });
-    const data=await response.json();
-    console.log(data);
+  function addMovieHandler(movie) {
+    console.log(movie);
   }
 
   let content = <p>Found no movies.</p>;
